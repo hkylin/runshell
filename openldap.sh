57f67e655437e7dd7063e9dad603675e4014cc43
aaron.wang <649386520@qq.com>
2017-11-30 21:45:50 +0800

    Create openldap.sh

---
+++ openldap.sh
@@ -0,0 +1,9 @@
+docker run -d -p 389:389 \
+-v /Users/aaron/data/openldap/data:/var/lib/ldap \
+-v /Users/aaron/data/openldap/config:/etc/ldap/slapd.d \
+--env LDAP_ORGANISATION="pbc" \
+--env LDAP_DOMAIN="cn.gov.pbc" \
+--env LDAP_ADMIN_PASSWORD="1qaz@WSX" \
+--name openldap-server \
+--restart=always \
+--detach osixia/openldap
