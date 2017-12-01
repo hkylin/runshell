a9ad020613c7a483a0ddf1ed5ededd1a7c508af8
aaron.wang <649386520@qq.com>
2017-12-01 10:56:04 +0800

    Update openldap.sh

--- openldap.sh
+++ openldap.sh
@@ -2,7 +2,7 @@ docker run -d -p 389:389 \
 -v /Users/aaron/data/openldap/data:/var/lib/ldap \
 -v /Users/aaron/data/openldap/config:/etc/ldap/slapd.d \
 --env LDAP_ORGANISATION="pbc" \
---env LDAP_DOMAIN="cn.gov.pbc" \
+--env LDAP_DOMAIN="pbc.gov.cn" \
 --env LDAP_ADMIN_PASSWORD="1qaz@WSX" \
 --name openldap-server \
 --restart=always \
